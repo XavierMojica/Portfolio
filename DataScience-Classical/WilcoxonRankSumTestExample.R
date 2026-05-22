ae743974f8fb5c90c9617215cb845476ac07d4e6
XavierMojica <112227722+XavierMojica@users.noreply.github.com>
2026-05-22 18:48:35 -0500

    Add files via upload

---
+++ DataScience-Classical/WilcoxonRankSumTestExample.R
@@ -0,0 +1,23 @@
+library(stats)
+
+#DF, testMethod using new or traditional and scores from individual subjects 
+testMethod = c("New", "New", "New", "New", "Trad", "Trad", "Trad")
+scores = c(37, 49, 55, 77, 23, 31, 46)
+
+# making a dataframe from 2 vectors
+mydata = data.frame(vars = testMethod, nums = scores)
+
+print(mydata)
+
+#Wilcoxon Rank Sum Test 
+wilcox.test(scores~testMethod, alternative = "two.sided")
+#wilcoxon rank sum test with exact 
+wilcox.test(scores~testMethod, exact = TRUE, alternative = 'two.sided')
+
+
+#Ho: The median of New Method Scores = The median of the Traditional Method Scores 
+#H1: The median of New Method Scores > The median of the Traditional Method Scores 
+
+#There is sufficient evidence at the alpha = 0.1 level of significance (p-value = .0571 for the exact test for the Wilcoxon Rank Sum Test) 
+#to suggest that the median of IBM employees that were given the New Method is greater than the median of those who took the test having had 
+#the Traditional Method of instruction. 
