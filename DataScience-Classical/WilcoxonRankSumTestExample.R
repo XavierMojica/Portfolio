7d22c6ca3a50ef2b5cdb5f3a247c45a30a3992c2
XavierMojica <112227722+XavierMojica@users.noreply.github.com>
2026-05-22 19:30:36 -0500

    Fix data frame creation and formatting in R script

--- DataScience-Classical/WilcoxonRankSumTestExample.R
+++ DataScience-Classical/WilcoxonRankSumTestExample.R
@@ -1,16 +1,17 @@
 library(stats)
 
 #DF, testMethod using new or traditional and scores from individual subjects 
-testMethod = c("New", "New", "New", "New", "Trad", "Trad", "Trad")
 scores = c(37, 49, 55, 77, 23, 31, 46)
+testMethod = c("New", "New", "New", "New", "Trad", "Trad", "Trad")
+
 
 # making a dataframe from 2 vectors
-mydata = data.frame(vars = testMethod, nums = scores)
+mydata = data.frame(nums = scores,vars = testMethod)
 
 print(mydata)
 
 #Wilcoxon Rank Sum Test 
-wilcox.test(scores~testMethod, alternative = "greater")#two.sided, less, greater
+wilcox.test(scores~testMethod, alternative = "greater") #two.sided, less, greater
 #wilcoxon rank sum test with exact 
 wilcox.test(scores~testMethod, exact = TRUE, alternative = 'greater') #careful with EXACT bc can crash with more Ns
 
